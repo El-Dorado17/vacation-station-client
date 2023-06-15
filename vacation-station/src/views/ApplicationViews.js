e79cbdc61a2a625b903ecbd0d2c4f9a8e0883417
Ely Dorado <2018dorado@gmail.com>
2023-06-15 10:50:28 -0500

    login & register work

--- vacation-station/src/views/ApplicationViews.js
+++ vacation-station/src/views/ApplicationViews.js
@@ -2,11 +2,12 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 
-//<Route path="/login" element={<Login />} /> //!Insert after register works!
+
 
 
 export const ApplicationViews = () => {
     return <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
     </Routes>
 }  
